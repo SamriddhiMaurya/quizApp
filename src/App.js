789b05aea6cf7905da629cec8bef5eb7063b986d
SamriddhiMaurya <samriddimaurya9005@gmail.com>
2024-02-12 22:23:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Settings from "./pages/Settings";
+import Questions from "./pages/Questions";
+import FinalScreen from "./pages/FinalScreen";
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+function App() {
+  return (
+    <Router>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Typography variant="h2" fontWeight="bold">
+                    Quiz App
+                  </Typography>
+                  <Settings />
+                </>
+              }
+            />
+            <Route path="/questions" element={<Questions />} />
+            <Route path="/score" element={<FinalScreen />} />
+          </Routes>
+        </Box>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
